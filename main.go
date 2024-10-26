27ce7efa2845fbff1c61e648b195c859f3c106ab
Shrinav Loka <lokashrinav@gmail.com>
2024-10-26 14:37:18 -0400

    Finished URL Shortener

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,13 +8,19 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	smth := map[string]string {
+	urlMap := map[string]string {
 		"/url": "https://gophercises.com/",
 		"/dog": "https://github.com/lokashrinav?tab=repositories",
 	}
 
 	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		
+		path := r.URL.Path
+		value, ok := urlMap[path]; 
+		if ok {
+			http.Redirect(w, r, value, http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	})
 
 	http.ListenAndServe(":3000", mux)
