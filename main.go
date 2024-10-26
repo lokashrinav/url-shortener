f51d6b666b7df2c7c09ae2b05b4598425238a69b
Shrinav Loka <lokashrinav@gmail.com>
2024-10-26 14:26:03 -0400

    Finishing Redirection Efforts

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func checkInMap() {
-
-}
-
 
 func main() {
-	mux := newRouter()
+	mux := http.NewServeMux()
+
+	smth := map[string]string {
+		"/url": "https://gophercises.com/",
+		"/dog": "https://github.com/lokashrinav?tab=repositories",
+	}
+
+	mux.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		
+	})
+
+	http.ListenAndServe(":3000", mux)
 	
 }
